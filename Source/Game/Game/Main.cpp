16c84d9ab478f8364e30c81f44285d277b8039fc
Kia Tucker <64986807+that-writer-16@users.noreply.github.com>
2023-06-28 14:22:41 -0600

    File System Completed.

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -5,9 +5,26 @@ using namespace std;
 
 int main()
 {
-
+	cout << "Get File Path: "; 
 	cout << kiko::getFilePath() << endl;
+
+	kiko::setFilePath("Assets");
+	cout << "Get File Path. New file path set: ";
+	cout << kiko::getFilePath() << endl;
+
+	size_t size;
+	cout << "Get File Size: ";
+	kiko::getFileSize("game.txt", size);
+	cout << size << endl;
+
+
+	std::string s;
+	cout << "Read File: ";
+	kiko::readFile("game.txt", s);
+	cout << s << endl;
+
 	kiko::seedRandom((unsigned int)time(nullptr));
+	cout << "Random: ";
 	for (int i = 0; i < 5; i++) 
 	{
 		cout << kiko::random(1,5) << endl;
